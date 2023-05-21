13828b0de459010f5a3e6f72987fe3839d3eb68f
Larision <fjavigar@hotmail.com>
2023-05-21 21:50:16 +0200

    copyright update

--- src/dali/plugin/country/es.py
+++ src/dali/plugin/country/es.py
@@ -1,4 +1,4 @@
-# Copyright 2022 eprbell
+# Copyright 2023 Larision
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
